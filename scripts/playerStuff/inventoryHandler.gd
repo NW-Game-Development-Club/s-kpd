e781dcf9e4e89d6cce0441b25c9bf6de16dc1368
LUKE LANNING <pacman4021@outlook.com>
2025-03-02 13:49:27 -0600

    Fixed selecting a different item with an item already selected. Added the ability to drop items from your inventory

--- scripts/playerStuff/inventoryHandler.gd
+++ scripts/playerStuff/inventoryHandler.gd
@@ -2,61 +2,91 @@ extends Node3D
 
 @export var grabRange: int = 5 # The maximum range that the player can grab objects
 
-@export var inventory: Array[Item] = []
-@export var weaponSets: Array[Item] = []
+@export var inventory: Array[Item] = [] # All the items that the player is carrying
+@export var weaponSets: Array[Item] = [] # The weapon sets the player can cycle between
 
-@export var toolShell: ToolShell
-@export var itemList: ItemList
+@export var toolShell: ToolShell # The item that handles all the weapons/tool stuff
+
+@export var worldItemPrefab: PackedScene
 
 @onready var camera = get_viewport().get_camera_3d()
-@onready var itemNameBox:Label = itemList.get_parent().get_node("SelectedItemName")
-@onready var itemDescBox:Label = itemList.get_parent().get_node("SelectedItemDesc") 
-@onready var weaponSet1Box:Label = itemList.get_parent().get_node("WeaponSet1Value")
-@onready var weaponSet2Box:Label = itemList.get_parent().get_node("WeaponSet2Value") 
+
+@export_group("Display")
+@export var itemList: ItemList # The UI element that handles the items displayed in a list
+@export var selectedItemNameBox:Label # The label to display the selected item's name
+@export var selectedItemDescBox:Label # The label to display the selected item's description
+@export var weaponSet1Box:Label # The label to display the weapon-set-one's name
+@export var weaponSet2Box:Label # The label to display the weapon-set-two's name
+
+@export_group("Buttons")
+@export var dropButton:Button
 
 var currentWeaponSet: int = 0
+var selectedItem: Item
 
 func loadInventory():
+	itemList.clear()
+	for item:Item in inventory:
+		itemList.add_item(item.itemName)
+	itemList.get_parent().visible = true
+
+func toggleInventory():
 	if itemList.get_parent().visible:
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Locks the mouse to the screen
 		itemList.get_parent().visible = false
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) # Locks the mouse to the screen
-		itemList.clear()
-		for item:Item in inventory:
-			itemList.add_item(item.itemName)
-		itemList.get_parent().visible = true
+		loadInventory()
 
-func getInventoryItemFromName(nameString):
+func getInventoryItemFromName(nameString) -> Item:
 	for item:Item in inventory:
 		if item.itemName == nameString:
 			return item
 	return null
 
+func changeSelectionDisplays():
+	selectedItem = null
+	selectedItemNameBox.text = ""
+	selectedItemDescBox.text = ""
+	dropButton.disabled = true
+
+# When the player clicks on any item, it could select a new item, or deselect an item we have already clicked on
 func onInventoryItemSelect(selectedItemIndex):
-	# If the item we clicked is not already selected, select it and update the inspection box
-	print(str(itemNameBox.text))
-	if itemNameBox.text == "Name: ": 
+	# If no item is selected or we click a different item, select it and update the inspection box
+	print(str(selectedItemNameBox.text))
+	var selectedItemName = itemList.get_item_text(selectedItemIndex)
+	if selectedItem == null || getInventoryItemFromName(selectedItemName) != selectedItem: 
 		print("Selected item #"+str(selectedItemIndex))
 		
-		var selectedItemName = itemList.get_item_text(selectedItemIndex)
+		selectedItem = getInventoryItemFromName(selectedItemName)
 		
-		var selectedItem = getInventoryItemFromName(selectedItemName)
-		
-		itemNameBox.text = "Name: "+ selectedItem.itemName
-		itemDescBox.text = "Desciption: "+ selectedItem.desciption
+		selectedItemNameBox.text = selectedItem.itemName
+		selectedItemDescBox.text = selectedItem.desciption
+		dropButton.disabled = false
+	
 	# If the item we clicked is selected, deselect it
 	else:
 		itemList.deselect(selectedItemIndex)
-		itemNameBox.text = "Name: "
-		itemDescBox.text = "Desciption: "
+		
+		changeSelectionDisplays()
+		
 		print("Deselected item #"+str(selectedItemIndex))
 
+# When the player clicks on the itemlist where no items are, this deselects all selected items
 func onInventoryItemDeselectAll(pos: Vector2, mouseButtonIndex: int):
 	itemList.deselect_all()
-	itemNameBox.text = "Name: "
-	itemDescBox.text = "Desciption: "
+	
+	changeSelectionDisplays()
+	
 	print("Deselected all items")
+
+# Sets the passed item so it is in the weapon set based on the provided number
+func onWeaponSetSet(toolToSet:Item, weaponSetNumber: int):
+	weaponSetNumber -= 1 # Subtract one to make it zero indexed
+	if selectedItem && selectedItem is Tool:
+		weaponSets.append(selectedItem)
+		weaponSet1Box.text = weaponSets[weaponSetNumber].itemName
+		print("Added to weapon set: "+ str(selectedItem.itemName))
 
 func _input(event):
 	if event.is_action_pressed("pick_up_item"):
@@ -79,8 +109,7 @@ func _input(event):
 				
 			# Call the world item to do its pick up stuff such as hiding itself
 			result.collider.onPickup(self)
-			
-
+		
 	if event.is_action_pressed("equip_weapon"):
 		if weaponSets && weaponSets.size() > 0:
 			if toolShell.equippedTool == null:
@@ -91,4 +120,18 @@ func _input(event):
 			print("No weapons equipped")
 			
 	if event.is_action_pressed("open_inventory"):
-		loadInventory()
+		toggleInventory()
+
+func dropItem(item:Item):
+	var droppedItem:worldItem = worldItemPrefab.instantiate()
+	droppedItem.item = selectedItem
+	get_tree().root.add_child(droppedItem)
+	droppedItem.global_position = camera.global_position - camera.global_transform.basis.z
+	
+	inventory.remove_at(inventory.find(selectedItem))
+	weaponSets.remove_at(weaponSets.find(selectedItem))
+	loadInventory()
+	changeSelectionDisplays()
+
+func _on_drop_button_pressed() -> void:
+	dropItem(selectedItem)
